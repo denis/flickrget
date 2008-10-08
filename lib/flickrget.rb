08cc3ec255d51d70bdc4d412e97d53627d5872ec
Denis Barushev <barushev@gmail.com>
2008-10-08 14:46:41 +0300

    Now downloads all photos (not only first 500)

--- lib/flickrget.rb
+++ lib/flickrget.rb
@@ -13,17 +13,17 @@ class Flickrget
       # Get public photos for the given user
       when 'people'
         @url  = "flickr.people.getPublicPhotos&user_id=#{id}"
-        @path = '/rsp/photos/photo'
+        @path = '/rsp/photos'
 
       # Get photos in the given set
       when 'photosets'
         @url  = "flickr.photosets.getPhotos&photoset_id=#{id}"
-        @path = '/rsp/photoset/photo'
+        @path = '/rsp/photoset'
 
       # Get favorite public photos for the given user
       when 'favorites'
         @url  = "flickr.favorites.getPublicList&user_id=#{id}"
-        @path = '/rsp/photos/photo'
+        @path = '/rsp/photos'
 
       else
         raise
@@ -44,9 +44,22 @@ class Flickrget
   private
 
   def photos_urls
-    doc = Hpricot(open(@url))
-    doc.search(@path).map do |photo|
-      "http://farm#{photo.get_attribute('farm')}.static.flickr.com/#{photo.get_attribute('server')}/#{photo.get_attribute('id')}_#{photo.get_attribute('originalsecret')}_o.#{photo.get_attribute('originalformat')}"
+    result, page, pages  = [], 1, 1
+
+    while page <= pages do
+      doc = Hpricot(open("#{@url}&page=#{page}"))
+
+      doc.search("#{@path}/photo").each do |p|
+        result << "http://farm#{p['farm']}.static.flickr.com/#{p['server']}/#{p['id']}_#{p['originalsecret']}_o.#{p['originalformat']}"
+      end
+
+      if page == 1
+        pages = doc.at(@path)['pages'].to_i
+      end
+      page += 1
     end
+
+    puts "#{result.size} photo(s) founded\n"
+    result
   end
 end
